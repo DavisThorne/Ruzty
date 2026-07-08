9906e84b089aa85398ed3648cc636aad2bb71402
Davis Thorne <davis.thorne2006@hotmail.com>
2026-07-08 02:54:49 +0100

    Added new helper functions, new opcodes and formatting
    Implemented new flag helper functions inside of cpu/mod.rs and a couple new opcodes inside of cpu/opcodes.rs

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,34 +1,35 @@
 #[doc(inline)]
-
 use std::io;
-use crate::bus::BUS;
 pub mod flags;
+mod opcodes;
+use crate::bus::BUS;
+use crate::cpu::flags::*;
 
-const Z_FLAG: u16 = 0x0080;
-const N_FLAG: u16 = 0x0040;
-const H_FLAG: u16 = 0x0020;
-const C_FLAG: u16 = 0x0010;
+// const Z_FLAG: u16 = 0x0080;
+// const N_FLAG: u16 = 0x0040;
+// const H_FLAG: u16 = 0x0020;
+// const C_FLAG: u16 = 0x0010;
 
 /// Representation of the Sharp LR35902 used in the GameBoy
 /// The CPU contains 6 registers an opcode table and the main memory of the system
 /// This struct is responsible for Fetching, Decoding and Executing instructions
 pub struct CPU {
     /// The Program Counter (PC) is responisble storing where in "memory" the system is at, it is an index inside the program vector
-    pc: u16,    //Program Counter
+    pc: u16, //Program Counter
     /// The AF register is 2, u8 bit registers combined into 1, u16 bit register responsible for the Accumulator (High 8 bits) and the CPU flags (Low 8 bits)
-    af: u16,    //Accumulator&Flags Hi=Accumulator Lo=Flags
+    af: u16, //Accumulator&Flags Hi=Accumulator Lo=Flags
     /// The BC register is 2, u8 bit registers combined into 1, u16 bit register where the higher 8 bits are the B register and the lower 8 bits are the C register
-    bc: u16,    //B&C Hi=B Lo=C
+    bc: u16, //B&C Hi=B Lo=C
     /// The BC register is 2, u8 bit registers combined into 1, u16 bit register where the higher 8 bits are the D register and the lower 8 bits are the E register
-    de: u16,    //D&E Hi=D Lo=E
-    /// The BC register is 2, u8 bit registers combined into 1, u16 bit register where the higher 8 bits are the H register and the lower 8 bits are the L register
-    hl: u16,    //H&L Hi=H Lo=L
+    de: u16, //D&E Hi=D Lo=E
+    /// The HL register is 2, u8 bit registers combined into 1, u16 bit register where the higher 8 bits are the H register and the lower 8 bits are the L register
+    hl: u16, //H&L Hi=H Lo=L
     /// The SP register is 1, u16 bit register that stores the memory address for the top of the stack, it automatically decrements instelf before pushing something onto the stack and vice versa with popping on the stack
-    sp: u16,     //Stack Pointer
+    sp: u16, //Stack Pointer
     /// Memory is a u8 vector that stores the program instructions, it is temporary and is to be replaced with a proper memory map and passed in via cartridge/ROM data
     stop: bool,
     pub bus: BUS,
-    opcode_table: [Opcode; 256]
+    opcode_table: [Opcode; 256],
 }
 
 type Opcode = fn(&mut CPU);
@@ -50,26 +51,42 @@ impl CPU {
             sp: 0,
             stop: false,
             bus: bus,
-            opcode_table: [CPU::op_null; 256]
+            opcode_table: [CPU::op_null; 256],
         }
     }
 
     // temp set to pub for testing
-    pub fn flip_flag(&mut self, flag: u16){
+    pub fn flip_flag(&mut self, flag: u16) {
         self.af ^= flag;
+    }
+
+    fn set_flag(&mut self, flag: u16) {
+        self.af |= flag;
+    }
+
+    fn clear_flag(&mut self, flag: u16) {
+        self.af &= !flag;
+    }
+
+    fn check_hc(&mut self, a: u8, b: u8, data: u8) {
+        if ((a ^ b ^ data) & 0x10) != 0 {
+            self.set_flag(H_FLAG);
+        } else {
+            self.clear_flag(H_FLAG);
+        }
     }
 
     fn fetch_u8(&mut self) -> u8 {
         //let byte = self.memory.read_mem([self.pc as usize]);
         let byte = self.bus.read(self.pc);
-        self.pc+=1;
+        self.pc += 1;
         return byte;
     }
 
     fn fetch_u16(&mut self) -> u16 {
         let low = self.fetch_u8() as u16;
         let high = self.fetch_u8() as u16;
-        return (high<<8) | low;
+        return (high << 8) | low;
     }
 
     fn fetch_register_high(&mut self, register: u16) -> u8 {
@@ -95,13 +112,14 @@ impl CPU {
         handler(self);
     }
     /// Run
-    pub fn run(&mut self, debug: bool){
+    pub fn run(&mut self, debug: bool) {
         self.pc = 0x0000;
 
         if debug {
             while !self.stop {
                 let opcode = self.fetch_u8();
-                println!("
+                println!(
+                    "
                     Opcode: {o:#02X}
                     PC: {pc:#04X} : {pc}
                     AF: {af:#04X} : {af}
@@ -109,19 +127,23 @@ impl CPU {
                     DE: {de:#04X} : {de}
                     HL: {hl:#04X} : {hl}
                     SP: {sp:#04X} : {sp}",
-                    o=opcode,pc=self.pc,af=self.af,bc=self.bc,de=self.de,hl=self.hl,sp=self.sp);
-                wait_for_enter(); 
+                    o = opcode,
+                    pc = self.pc,
+                    af = self.af,
+                    bc = self.bc,
+                    de = self.de,
+                    hl = self.hl,
+                    sp = self.sp
+                );
+                wait_for_enter();
                 //let opcode = self.fetch_u8();
-                self.execute(opcode);        
+                self.execute(opcode);
             }
-        }
-        else {
+        } else {
             while !self.stop {
                 let opcode = self.fetch_u8();
-                self.execute(opcode);         
+                self.execute(opcode);
             }
         }
     }
 }
-
-mod opcodes;
--- src/cpu/opcodes.rs
+++ src/cpu/opcodes.rs
@@ -1,4 +1,6 @@
 use super::CPU;
+use crate::cpu::flags::*;
+
 #[doc(hidden)]
 impl CPU {
     pub fn op_null(&mut self) {
@@ -13,58 +15,72 @@ impl CPU {
         println!("op_stop hit");
         self.stop = true;
     }
-    pub fn op_jr_nz_s8(&mut self){
+    pub fn op_jr_nz_s8(&mut self) {
         if (self.af & crate::cpu::flags::Z_FLAG) == 0 {
-            self.pc+=0x07; // only jump by 7 because cpu.fetch increments PC by 1
+            self.pc += 0x07; // only jump by 7 because cpu.fetch increments PC by 1
         }
     }
-    pub fn op_jr_nc_s8(&mut self){
+    pub fn op_jr_nc_s8(&mut self) {
         if (self.af & crate::cpu::flags::C_FLAG) == 0 {
-            self.pc+=0x07;
+            self.pc += 0x07;
         }
     }
-    pub fn op_ld_b_b(&mut self){
+    pub fn op_ld_b_b(&mut self) {
         let data = self.fetch_register_high(self.bc);
         CPU::set_register_high(&mut self.bc, data);
     }
-    pub fn op_ld_d_b(&mut self){
+    pub fn op_ld_d_b(&mut self) {
         let data = self.fetch_register_high(self.bc);
         CPU::set_register_high(&mut self.de, data);
     }
-    pub fn op_ld_h_b(&mut self){
+    pub fn op_ld_h_b(&mut self) {
         let data = self.fetch_register_high(self.bc);
         CPU::set_register_high(&mut self.hl, data);
     }
-    pub fn op_ld_hl_b(&mut self){
+    pub fn op_ld_hl_b(&mut self) {
         let data = self.fetch_register_high(self.bc);
         self.bus.write(self.hl, data);
     }
-    pub fn op_ld_bc_d16(&mut self){
+    pub fn op_ld_bc_d16(&mut self) {
         //let param = ((self.memory[(self.pc+1) as usize] as u16) << 8) | self.memory[(self.pc) as usize] as u16;
         let param = self.fetch_u16() as u16;
         self.bc = param;
     }
-    pub fn op_ld_de_d16(&mut self){
+
+    pub fn op_add_a_b(&mut self) {
+        let b = self.fetch_register_high(self.bc);
+        let a = self.fetch_register_high(self.af);
+        let data = a + b;
+        if data == 0x0000 {
+            self.set_flag(Z_FLAG);
+        }
+        self.check_hc(a, b, data);
+        CPU::set_register_high(&mut self.af, data);
+    }
+
+    pub fn op_ld_de_d16(&mut self) {
         //let param = ((self.memory.memory[(self.pc+1) as usize] as u16) << 8) | self.memory.memory[(self.pc) as usize] as u16;
         let param = self.fetch_u16() as u16;
         self.de = param;
     }
-    pub fn op_ld_hl_d16(&mut self){
+    pub fn op_ld_hl_d16(&mut self) {
         let param = self.fetch_u16() as u16;
         self.hl = param;
     }
-    pub fn op_ld_sp_d16(&mut self){
-        let param= self.fetch_u16() as u16;
+    pub fn op_ld_sp_d16(&mut self) {
+        let param = self.fetch_u16() as u16;
         self.sp = param;
     }
 
-    pub fn build_opcode_table(&mut self){
+    pub fn build_opcode_table(&mut self) {
         self.opcode_table[0x00] = CPU::op_nop;
         self.opcode_table[0x10] = CPU::op_stop;
         self.opcode_table[0x20] = CPU::op_jr_nz_s8;
         self.opcode_table[0x30] = CPU::op_jr_nc_s8;
         self.opcode_table[0x40] = CPU::op_ld_b_b;
         self.opcode_table[0x50] = CPU::op_ld_d_b;
+        self.opcode_table[0x60] = CPU::op_ld_h_b;
+        self.opcode_table[0x70] = CPU::op_ld_hl_b;
         self.opcode_table[0x01] = CPU::op_ld_bc_d16;
         self.opcode_table[0x11] = CPU::op_ld_de_d16;
         self.opcode_table[0x21] = CPU::op_ld_hl_d16;
@@ -72,5 +88,3 @@ impl CPU {
         println!("Opcode table Built")
     }
 }
-
-
